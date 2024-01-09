98f5b3d3a0e6caa5fb73a0db501f34485ad8e1a6
vovkatom <36505066+vovkatom@users.noreply.github.com>
2024-01-09 23:11:27 +0200

    reNew

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,14 +10,25 @@ import Massege from '../Massege/Massege';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem("my-contacts"));
+    if (contacts?.length) {
+        this.setState({
+          contacts,
+        })
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts.length !== contacts.length) {
+        localStorage.setItem("my-contacts", JSON.stringify(this.state.contacts));
+    }
+  }
 
   addContact = ({ name, number }) => {
     const { contacts } = this.state;
